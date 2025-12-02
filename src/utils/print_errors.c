dfc758f08f67ef969fc401c9b73069be519c3011
Pauline Froidure <pafroidu@student.42.fr>
2025-12-02 16:59:28 +0100

    chore: add doxygen com

--- src/utils/print_errors.c
+++ src/utils/print_errors.c
@@ -1,5 +1,15 @@
 #include "cub3d.h"
 
+/**
+ * @brief Print an error message with optional parts.
+ *
+ * This function prints "Error" followed by up to three optional
+ * message strings. It also applies color formatting (red + reset).
+ *
+ * @param p1 First part of the error message (can be NULL).
+ * @param p2 Second part of the error message (can be NULL).
+ * @param p3 Third part of the error message (can be NULL).
+ */
 void	print_errors(char *p1, char *p2, char *p3)
 {
 	printf("%s", RED);
