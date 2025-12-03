f546e0e4110cfe5dae8193f89b10bd4b31388266
Pauline Froidure <pafroidu@student.42.fr>
2025-12-03 14:29:40 +0100

    chore: experiment with mlx

--- tests/unit/test_init_mlx.c
+++ tests/unit/test_init_mlx.c
@@ -13,12 +13,20 @@ int	main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_game	test;
+	int		i;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 400, 600, "Hello !");
 	test.img = mlx_new_image(mlx, 400, 600);
 	test.img_addr = mlx_get_data_addr(test.img, &test.img_bpp, &test.img_line_len, &test.img_endian);
-	my_mlx_pixel_put(&test, 5, 5, 0x00FF0000);
+
+	i = 0;
+	while (i < 20)
+	{
+		my_mlx_pixel_put(&test, 5 + i, 5 + i, 0x00FF0000);
+		i++;
+	}
+
 	mlx_put_image_to_window(mlx, mlx_win, test.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
