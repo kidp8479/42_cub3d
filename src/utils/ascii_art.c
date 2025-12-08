c0157a4269a769401e13df8b10c324b68a2e594d
Pauline Froidure <pafroidu@student.42.fr>
2025-12-08 22:53:21 +0100

    feature: fix color in ascci art

--- src/utils/ascii_art.c
+++ src/utils/ascii_art.c
@@ -3,7 +3,7 @@
 void	print_ascii_art_hello(void)
 {
 	printf("Welcome to\n");
-	printf(GRN);
+	printf(BR_GRN);
 	printf(" ██████╗██╗   ██╗██████╗ ██████╗ ██████╗\n");
 	printf("██╔════╝██║   ██║██╔══██╗╚════██╗██╔══██╗\n");
 	printf("██║     ██║   ██║██████╔╝ █████╔╝██║  ██║\n");
@@ -11,6 +11,8 @@ void	print_ascii_art_hello(void)
 	printf("╚██████╗╚██████╔╝██████╔╝██████╔╝██████╔╝\n");
 	printf(" ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝\n");
 	printf(RESET);
+	printf(BR_YEL);
 	printf("use WASD to move - left/right arrow to rotate camera\n");
 	printf("press ESC or click the top right window cross to exit game\n\n");
+	printf(RESET);
 }
