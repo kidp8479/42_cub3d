ba051aadc428b3b6c1eac8eea6b868f1e7c8170b
Pauline Froidure <pafroidu@student.42.fr>
2025-12-08 16:21:29 +0100

    feature: code basic events related functions
    - cleanup function that cleans all the data when the user presses ESC
    key or click the red cross on top of the windows
    - prepare stub functions for player movements, for now, while waiting
    raycasting part, they log their action into the console

---
+++ src/events/player actions_rotate.c
@@ -0,0 +1,23 @@
+#include "cub3d.h"
+
+/**
+  * @brief Rotates camera to the left
+  *
+  * @param game Pointer to game structure
+*/
+void	rotate_left(t_game *game)
+{
+	(void)game;
+	printf("DEBUG: Rotating LEFT\n");
+}
+
+/**
+  * @brief Rotates camera to the right
+  *
+  * @param game Pointer to game structure
+*/
+void	rotate_right(t_game *game)
+{
+	(void)game;
+	printf("Rotating RIGHT\n");
+}
---
+++ src/events/player_actions_move.c
@@ -0,0 +1,46 @@
+#include "cub3d.h"
+
+/**
+  * @brief Moves player forward in current direction
+  *
+  * @param game Pointer to game structure
+*/
+void	move_forward(t_game *game)
+{
+	(void)game;
+	printf(" DEBUG: Moving FORWARD\n");
+}
+
+/**
+  * @brief Moves player backward (opposite of current direction)
+  *
+  * @param game Pointer to game structure
+*/
+void	move_backward(t_game *game)
+{
+	(void)game;
+	printf("DEBUG: Moving BACKWARD\n");
+}
+
+/**
+  * @brief Strafes player to the left (perpendicular to direction)
+  *
+  * @param game Pointer to game structure
+*/
+void	strafe_left(t_game *game)
+{
+	(void)game;
+	printf("DEBUG: Strafing LEFT\n");
+}
+
+/**
+  * @brief Strafes player to the right (perpendicular to direction)
+  *
+  * @param game Pointer to game structure
+*/
+void	strafe_right(t_game *game)
+{
+	(void)game;
+	printf("DEBUG: Strafing RIGHT\n");
+}
+
