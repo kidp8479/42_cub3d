3ed4d1cac5570be142c89df3cdf254af3cc4ea08
Pauline Froidure <pafroidu@student.42.fr>
2025-12-08 16:22:21 +0100

    feature: I know I can't help it: ASCII ART time
    it's very basic, I'll see what I can do better later

---
+++ src/utils/ascii_art.c
@@ -0,0 +1,16 @@
+#include "cub3d.h"
+
+void	print_ascii_art_hello(void)
+{
+	printf("Welcome to\n");
+	printf(GRN);
+	printf(" ██████╗██╗   ██╗██████╗ ██████╗ ██████╗\n");
+	printf("██╔════╝██║   ██║██╔══██╗╚════██╗██╔══██╗\n");
+	printf("██║     ██║   ██║██████╔╝ █████╔╝██║  ██║\n");
+	printf("██║     ██║   ██║██╔══██╗ ╚═══██╗██║  ██║\n");
+	printf("╚██████╗╚██████╔╝██████╔╝██████╔╝██████╔╝\n");
+	printf(" ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝\n");
+	printf(RESET);
+	printf("use WASD to move - left/right arrow to rotate camera\n");
+	printf("press ESC or click the top right window cross to exit game\n\n");
+}
