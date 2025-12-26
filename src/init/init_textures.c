22685ab9ccf4f1712cb911c1c3f6a30a92b04773
Pauline Froidure <pafroidu@student.42.fr>
2025-12-26 23:41:48 +0100

    fix: remove misleading indentation

--- src/init/init_textures.c
+++ src/init/init_textures.c
@@ -60,7 +60,7 @@ int	init_textures(t_game *game)
 		if (load_texture(game, &game->textures[i], game->map.tex_paths[i])
 			!= EXIT_SUCCESS)
 			cleanup_textures(game);
-			return (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 		i++;
 	}
 	return (EXIT_SUCCESS);
